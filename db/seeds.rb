0a55034ad700c18cbdc7cb33b9b93db60824b76f
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-02-28 12:44:45 -0500

    Created City object and added cities to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+barcelona = City.new
+barcelona.city = "Barcelona"
+barecelona.state = nil
+barecelona.country = "Spain"
+barecelona.save()
+
+new_york_city = City.new
+new_york_city.city = "New York City"
+new_york_city.state = "New York"
+new_york_city.country = "United State"
+new_york_city.save()
+
+chicago = City.new
+chicago.city = "Chicago"
+chicago.state = "Illinois"
+chicago.country = "United State"
+chicago.save()
