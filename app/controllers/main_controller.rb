ef28b6733bbfdaad574442fd1469e4f2d786e696
Erin Hoffman <hoffm386@msu.edu>
2015-02-28 21:48:35 -0600

    Pulled list of businesses from yep
    Queries with session[:search], does fancy Oauth things, gets info from
    Yelp, parses it into things to populate temporary lists of top 5s

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -48,6 +48,53 @@ class MainController < ApplicationController
 		end
 
 		@city = City.find_by city: session[:search]
+
+		require 'yelp'
+		client = Yelp::Client.new({ consumer_key: "JSTcs5H0D2gItBX9NwtbOg",
+                            consumer_secret: "cp4-k3TIst1X8hw5JLrb8vXJpC4",
+                            token: "L4TPFk1ShjIXIozv7IM34iCbCUSpjwXV",
+                            token_secret: "Pm4yZ4B7QHB1ziMLWVMvGxRQCEo"
+                          })
+		
+		# finding the top 5 arts, food, and tours locations
+		arts_response = client.search(session[:search], {category_filter: "arts"}).businesses
+		food_response = client.search(session[:search], {category_filter: "food"}).businesses
+		tour_response = client.search(session[:search], {category_filter: "tours"}).businesses
+		
+		@arts_pins = {}
+		@food_pins = {}
+		@tour_pins = {}
+		for i in 0..4
+			arts_result = arts_response[i]
+			food_result = food_response[i]
+			tour_result = tour_response[i]
+
+			arts_pin = {}
+			food_pin = {}
+			tour_pin = {}
+
+			arts_pin["name"] = arts_result.name
+			food_pin["name"] = food_result.name
+			tour_pin["name"] = tour_result.name
+
+			arts_pin["rating"] = arts_result.rating
+			food_pin["rating"] = food_result.rating
+			tour_pin["rating"] = tour_result.rating
+
+			arts_pin["latitude"] = arts_result.location.coordinate.latitude
+			food_pin["latitude"] = food_result.location.coordinate.latitude
+			tour_pin["latitude"] = tour_result.location.coordinate.latitude
+
+			arts_pin["longitude"] = arts_result.location.coordinate.longitude
+			food_pin["longitude"] = food_result.location.coordinate.longitude
+			tour_pin["longitude"] = tour_result.location.coordinate.longitude
+			
+			@arts_pins[i] = arts_pin
+			@food_pins[i] = food_pin
+			@tour_pins[i] = tour_pin
+
+		end
+		
 	end
 
 	def blink
