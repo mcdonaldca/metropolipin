ed81dc0789bedcc927a8f64951855bc77e106947
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-02-28 19:35:24 -0500

    Fixed migration

--- db/migrate/20150228235837_update_pins.rb
+++ db/migrate/20150228235837_update_pins.rb
@@ -1,5 +1,6 @@
 class UpdatePins < ActiveRecord::Migration
   def change
+  		remove_column :pins, :title
   	  add_column :pins, :title, :string
   	  add_column :pins, :latitude, :string
   	  add_column :pins, :longitude, :string
