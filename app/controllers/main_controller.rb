54351f4fe17f38fd27f76d28e56671fdb1bff652
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-03-01 00:33:15 -0500

    Implemented yelp pins & sticky footer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -52,57 +52,63 @@ class MainController < ApplicationController
 		@tour_pins = {}
 		for i in 0..4
 
-			arts_result = arts_response[i]
-			food_result = food_response[i]
-			tour_result = tour_response[i]
-
-			arts_name = arts_result.name.downcase
-			food_name = food_result.name.downcase
-			tour_name = tour_result.name.downcase
-
-			arts_find = YelpPin.find_by name: arts_name 
-			food_find = YelpPin.find_by name: food_name
-			tour_find = YelpPin.find_by name: tour_name
-
-			if arts_find.nil?
-
-				arts_pin = YelpPin.new
-				arts_pin.name = arts_name
-			end
-
-			food_pin = YelpPin.new
-			tour_pin = YelpPin.new
 			#arts_pin = {}
 			#food_pin = {}
 			#tour_pin = {}
 
+			arts_result = arts_response[i]
+			food_result = food_response[i]
+			tour_result = tour_response[i]
 
-			
+			arts_title = arts_result.name.downcase
+			food_title = food_result.name.downcase
+			tour_title = tour_result.name.downcase
+			#arts_pin["name"] = arts_result.name
+			#food_pin["name"] = food_result.name
+			#tour_pin["name"] = tour_result.name
 
-			arts_pin.name = arts_result.rating
-			food_pin.name = food_result.rating
-			tour_pin.name = tour_result.rating
+			arts_pin = YelpPin.find_by title: arts_title
+			food_pin = YelpPin.find_by title: food_title
+			tour_pin = YelpPin.find_by title: tour_title
+
+			if arts_pin.nil?
+				arts_pin = YelpPin.new
+				arts_pin.title = arts_title
+				arts_pin.rating = arts_result.rating
+				arts_pin.latitude = arts_result.location.coordinate.latitude
+				arts_pin.longitude = arts_result.location.coordinate.longitude
+				arts_pin.save()
+			end
+
+			if food_pin.nil?
+				food_pin = YelpPin.new
+				food_pin.title = arts_title
+				food_pin.rating = food_result.rating
+				food_pin.latitude = food_result.location.coordinate.latitude
+				food_pin.longitude = food_result.location.coordinate.longitude
+				food_pin.save()
+			end
+
+			if tour_pin.nil?
+				tour_pin = YelpPin.new
+				tour_pin.title = arts_title
+				tour_pin.rating = tour_result.rating
+				tour_pin.latitude = tour_result.location.coordinate.latitude
+				tour_pin.longitude = tour_result.location.coordinate.longitude
+				tour_pin.save()
+			end
+
+
+
 			#arts_pin["rating"] = arts_result.rating
 			#food_pin["rating"] = food_result.rating
 			#tour_pin["rating"] = tour_result.rating
-
-			arts_pin.name = arts_result.rating
-			food_pin.name = food_result.rating
-			tour_pin.name = tour_result.rating
-			arts_pin["latitude"] = arts_result.location.coordinate.latitude
-			food_pin["latitude"] = food_result.location.coordinate.latitude
-			tour_pin["latitude"] = tour_result.location.coordinate.latitude
-
-			arts_pin.name = arts_result.rating
-			food_pin.name = food_result.rating
-			tour_pin.name = tour_result.rating
-			arts_pin["longitude"] = arts_result.location.coordinate.longitude
-			food_pin["longitude"] = food_result.location.coordinate.longitude
-			tour_pin["longitude"] = tour_result.location.coordinate.longitude
-
-			arts_pin.save()
-			food_pin.save()
-			tour_pin.save()
+			#arts_pin["latitude"] = arts_result.location.coordinate.latitude
+			#food_pin["latitude"] = food_result.location.coordinate.latitude
+			#tour_pin["latitude"] = tour_result.location.coordinate.latitude
+			#arts_pin["longitude"] = arts_result.location.coordinate.longitude
+			#food_pin["longitude"] = food_result.location.coordinate.longitude
+			#tour_pin["longitude"] = tour_result.location.coordinate.longitude
 			
 			@arts_pins[i] = arts_pin
 			@food_pins[i] = food_pin
